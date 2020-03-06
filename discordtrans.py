96b40654f7f3f332238fffb00e2b4ca0081f4622
Satyam Jha <10satyam.jha@gmail.com>
2020-03-06 13:48:37 +0530

    done

---
+++ discordtrans.py
@@ -0,0 +1,76 @@
+from translate import Translator
+import discord
+
+from discord.ext import commands
+
+# use your own token 
+token = "your token"
+
+#client ref
+bot = discord.Client()
+
+
+#ISO 639-1 language code search for ur language 
+translist = {1:"ko",2:"zh",3:"hi",4:"ja",5:"ar",6:"bh",7:"ru"}
+
+print("select lang")
+
+print(translist)
+
+lang = translist[int(input())]
+print(lang)
+
+
+#just to know bot is runing 
+
+@bot.event
+async def on_ready():
+	print("{} is running".format(bot.user))
+	
+
+#main function 
+
+
+def translator(text,lang):
+
+	tanslator = Translator(from_lang = "autodetect",to_lang = lang)
+
+
+
+
+	translation = tanslator.translate(text)
+
+
+	return translation
+
+
+
+
+#response on every msg
+
+@bot.event
+async def on_message(msg):
+	
+	testmsg = msg.content
+	if msg.author==bot.user:
+		return
+	
+	
+	else:
+		
+		#create a text channel called translator or change this according to channel u want to response on 
+		if msg.channel.name == "translator":
+
+			
+
+			reply = translator(msg.content,lang)
+			await msg.channel.send(reply)
+			
+		else :
+			
+
+			return
+
+
+
+bot.run(token)
